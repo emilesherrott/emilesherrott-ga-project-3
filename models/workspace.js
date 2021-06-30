827125033d55e129984c8d558dde40d065546c47
emilesherrott <emilesherrott@gmail.com>
2021-06-30 16:07:19 +0100

    Seed data and seed server start

---
+++ models/workspace.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongosse'
+
+//Define the schema for workspace, the schema is a blueprint for how we want each document for be formatted
+const workspaceSchema = new mongoose.Schema({
+  name : { type: String, required: true },
+  addressLineOne: { type: String, required: true, unique: true },
+  addressLineTwo: { type: String, required: true, unique: true },
+  city : { type: String, required: true },
+  postcode : { type: String, required: true, unique: true },
+  country : { type: String, required: true },
+  phoneNumber : { type: Number, required: true },
+  long : { type: String, required: true, unique: true },
+  lat : { type: String, required: true, unique: true },
+  link : { type: String, required: true, unique: true },
+  image : { type: String, required: true, unique: true },
+  textDescription : { type: String, required: true, unique: true, maxlength: 400 },
+  facilities : { type: Array }
+})
+
+export default mongoose.model('Workspace', workspaceSchema)
