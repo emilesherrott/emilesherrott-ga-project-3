a75bed3a0988283cf58728aef1783de2a680ecf8
emilesherrott <emilesherrott@gmail.com>
2021-07-06 09:02:36 +0100

    Create workspace component

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,31 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import Home from './components/common/Home.js'
-import Nav from './components/common/Nav.js'
-import Footer from './components/common/Footer.js'
-
-import Ideas from './components/Ideas.js'
-
-import Map from './components/common/Map.js'
-import Maptwo from './components/common/Maptwo.js'
-import Events from './components/common/EventSection/Events.js'
-import Login from './components/auth/Login.js'
+import Home from './components/common/Home'
+import Nav from './components/common/Nav'
+import Footer from './components/common/Footer'
+import Ideas from './components/Ideas'
+import Map from './components/common/Map'
+import Maptwo from './components/common/Maptwo'
+import Events from './components/common/EventSection/Events'
+import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 
-
+import WorkspacesNew from './components/common/Workspaces/WorkspacesNew'
 const App = () => {
   return (
     <Router>
       <Nav />
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/workspaces/new' component={WorkspacesNew} />
         <Route path="/ideas" component={Ideas} />
         <Route exact path='/events' component={Events} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-      </Switch>
-      
-      <Switch>
         <Route excact path='/map' component={Map}/>
-      </Switch>
-      <Switch>
         <Route excact path='/maptwo' component={Maptwo}/>
       </Switch>
-      
-      {/* <Switch>
-        <Route excact path='/' component={Footer}/>
-      </Switch> */}
-
       <Footer/>
-      
     </Router>
   )
 }
