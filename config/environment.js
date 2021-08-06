2e1d94f88f88532b6051802a8e4e18f4f7807fd8
emilesherrott <emilesherrott@gmail.com>
2021-08-06 22:46:13 +0100

    New head

--- config/environment.js
+++ config/environment.js
@@ -3,4 +3,4 @@ dotenv.config()
 
 export const port = process.env.PORT || 4000
 export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/workspaceapi' 
-export const secret = process.env.SECRET || 'our tiny smallest ever secret'
+export const secret = process.env.SECRET || 'our tiny smallest ever secret '
